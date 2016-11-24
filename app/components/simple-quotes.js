552cfc6e004a24efa3248995888f71be850e4553
Matt Payne <Payne@MattPayne.org>
2016-11-23 19:40:27 -0600

    Vonnegut quotes added.

--- app/components/simple-quotes.js
+++ app/components/simple-quotes.js
@@ -3,10 +3,28 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
 quotes: {data: [
-  {id: 1, text: 'I like eggs', author: 'radio skit'},
-  {id: 2, text: 'Be Nice!', author: 'Religion'}
+  {id: 0, text: `I urge you to please notice when you are happy,
+    and exclaim or murmur or think at some point,
+    “If this isn’t nice, I don’t know what is.”`,
+    author: `Kurt Vonnegut`},
+  {id: 1, text: `I tell you, we are here on Earth to fart around, and don't let anybody tell you different.`,
+  author: `Kurt Vonnegut`},
+  {id: 2, text: `We are what we pretend to be, so we must be careful about what we pretend to be.`,
+  author: `Kurt Vonnegut`},
+  {id: 3, text: `I want to stay as close to the edge as I can without going over.
+     Out on the edge you see all kinds of things you can't see from the center.`,
+  author: `Kurt Vonnegut`},
+  {id: 4, text: `Laughter and tears are both responses to
+    frustration and exhaustion. I myself prefer to laugh,
+    since there is less cleaning up to do afterward.`,
+  author: `Kurt Vonnegut`},
+  {id: 5, text: `Everything was beautiful and nothing hurt.`,
+  author: `Kurt Vonnegut`}
 ]},
-quote: {id: 3, text: 'Placeholder', author: 'Matt'},
+quote: {id: 0, text: `I urge you to please notice when you are happy,
+    and exclaim or murmur or think at some point,
+    “If this isn’t nice, I don’t know what is.”`,
+    author: `Kurt Vonnegut`},
 number: {value: 0},
 actions: {
   nextQuote() {
